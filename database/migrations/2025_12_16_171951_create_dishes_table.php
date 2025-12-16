7ba63664bff7a4c0bee12a5f0f0026ed1ac23dd3
Sodja1234 <franklinkabisayi@gmail.com>
2025-12-16 18:27:03 +0100

    feat: creation du model et de la migration Dish

---
+++ database/migrations/2025_12_16_171951_create_dishes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->string('image_path')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
