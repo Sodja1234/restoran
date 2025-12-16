d81b1e73e822e6a9a741ba933b9a48fe0b09504a
Sodja1234 <franklinkabisayi@gmail.com>
2025-12-16 18:32:46 +0100

    feat: creation du model et de la migration quoteRequest

---
+++ database/migrations/2025_12_16_172711_create_quote_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quote_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->string('client_email');
+            $table->string('client_phone')->nullable();
+            $table->date('event_date');
+            $table->string('event_location');
+            $table->integer('guest_count');
+            $table->text('details');
+            $table->enum('status', ['new', 'in_progress', 'quoted', 'closed'])->default('new');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quote_requests');
+    }
+};
