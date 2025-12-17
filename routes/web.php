dcee1a08b52708795622175fc34b400965f5df89
Sodja1234 <franklinkabisayi@gmail.com>
2025-12-17 11:14:22 +0100

    feat: creation de la route vers les ressource de DishController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DishController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,6 +21,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
     Route::resource('categories', CategoryController::class);
+    Route::resource('dishes', DishController::class);
 });
 
 require __DIR__.'/auth.php';
