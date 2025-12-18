cbc2cffa7d78abbffbb02ad0d9328db05f598f01
Sodja1234 <franklinkabisayi@gmail.com>
2025-12-18 05:40:24 +0100

    feat: ajout des routes quotes pour le public et l'admin

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DishController;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\QuoteRequestController;
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::post('/quote', [QuoteRequestController::class, 'store'])->name('quote.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,6 +23,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->prefix('admin')->group(function () {
     Route::resource('categories', CategoryController::class);
     Route::resource('dishes', DishController::class);
+    Route::get('/quotes', [QuoteRequestController::class, 'index'])->name('admin.quotes.index');
 });
 
 require __DIR__.'/auth.php';
